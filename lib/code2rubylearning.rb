9f137f15737c6d36ffb98c0b37694b72f5b36ca0
Catsquotl <catsquotl@gmail.com>
2012-12-20 19:49:31 +0100

    lost garbage classes and put code in main module

--- lib/code2rubylearning.rb
+++ lib/code2rubylearning.rb
@@ -1,7 +1,27 @@
 require 'clippy'
-require "code2rubylearning/version"
+require_relative "./code2rubylearning/version"
 
 module Code2rubylearning
+  @files = []
+  @switches = []
+  until ARGV.first.to_s.include?('-') == false
+    tmp_switch = ARGV.shift
+    @switches.push(tmp_switch)
+  end
+
+  until ARGV.empty?
+    tmp_file = ARGV.shift
+    @files.push(tmp_file)
+  end
+
+  def reformat_to_rubylearning file_name
+    File.open(file_name,"r") do |txt|
+      @file_text = txt.read
+    end
+    @file_text.gsub!('<' ,'&lt;')
+    @ref_file = "[code ruby]\n" << @file_text << '[/code]'
+  end
+
   # Your code goes here...
   extend self
   def copy_clipboard data
