2fd238d97c20dbcedb9567419de02632ae85351e
Catsquotl <catsquotl@gmail.com>
2012-12-20 23:56:48 +0100

    made basic tests for code2rl. did mimic the commandline options in the code itself.. needs to be improved upon as soon as I figure out how..

--- lib/code2rubylearning.rb
+++ lib/code2rubylearning.rb
@@ -1,7 +1,32 @@
 require 'clippy'
-require "code2rubylearning/version"
+require_relative "../lib/code2rubylearning/version"
 
 module Code2rubylearning
+  attr_reader :files,:switches
+  @files = []
+  @switches = []
+  args = %w{-i -s test.rb}
+  until args.first.to_s.include?('-') == false
+    tmp_switch = args.shift
+    @switches.push(tmp_switch)
+  end
+
+  until args.empty?
+    tmp_file = args.shift
+    @files.push(tmp_file)
+  end
+
+  class Reformat
+    def reformat_to_rubylearning file_name
+      File.open(file_name,"r") do |txt|
+        @file_text = txt.read
+      end
+      @file_text.gsub!('<' ,'&lt;')
+      @ref_file = "[code ruby]\n" << @file_text << '[/code]'
+    end
+  end
+
+
   # Your code goes here...
   extend self
   def copy_clipboard data
--- test/code2rubylearning_spec.rb
+++ test/code2rubylearning_spec.rb
@@ -1,6 +1,21 @@
-require "./test/minitest_helper"
-# require('./lib/code2rubylearning')
+require_relative "./minitest_helper"
+require_relative '../lib/code2rubylearning'
+
 
 describe Code2rubylearning do
- it"should skip this minispec"  
+    let (:c2rl){Code2rubylearning}
+
+  it "must be a module" do 
+    c2rl.must_be_kind_of Module
+  end
+
+  it "must hold an array of switches" do
+    c2rl::switches.must_be_kind_of Array
+    c2rl::switches[0].must_equal '-i'
+  end
+
+  it "must hold an Array of files" do
+   c2rl::files.must_be_kind_of Array 
+  end
+
 end
